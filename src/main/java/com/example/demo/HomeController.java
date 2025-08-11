daaced339ac8e4a6c0d609a7580247447507298a
geniusIN <rlaghddls9309@gmail.com>
2025-08-11 12:50:16 +0900

    main update

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -2,15 +2,16 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 public class HomeController {
 
 	@GetMapping("/")
+	@ResponseBody
 	public String main() {
-		return "hello springboot";
+		return "hello springboot!!!! docker build";
 	}
 	
 }
